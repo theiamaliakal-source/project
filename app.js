795e400b3b2ab6abc09b9b6d0aa7b3315edc5b09
theiamaliakal-source <theiamaliakal@gmail.com>
2026-04-25 18:55:00 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Initializing Lucide Icons on all pages dynamically mapped
+    if (window.lucide) {
+      lucide.createIcons();
+    }
+  });
