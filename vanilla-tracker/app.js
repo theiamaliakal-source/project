ddc44ceb371d5c241bff45014e3e4bc3320a66bb
theiamaliakal-source <theiamaliakal@gmail.com>
2026-04-25 18:51:55 +0530

    Delete vanilla-tracker directory

--- vanilla-tracker/app.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Initializing Lucide Icons on all pages dynamically mapped
-    if (window.lucide) {
-      lucide.createIcons();
-    }
-  });
